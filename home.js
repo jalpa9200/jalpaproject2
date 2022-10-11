4c7f1a0e2bc2aed2b492647a890e1cf88d9b88fd
jalpa9200 <jalpa9200@gmail.com>
2022-10-11 14:42:10 +0530

    code

---
+++ home.js
@@ -0,0 +1,29 @@
+//copy this code
+function loadCoupon() {
+    document.getElementById('coupon').style.visibility = 'visible';
+    //document.getElementById('main9').style.opacity = '1';
+    document.getElementById('nav').style.opacity = '0.4';
+    document.getElementById('roop').style.opacity = '0.4';
+    document.getElementById('categories').style.opacity = '0.4';
+    document.getElementById('veriouscategories').style.opacity = '0.4';
+    document.getElementById('collection').style.opacity = '0.4';
+    document.getElementById('offervideo').style.opacity = '0.4';
+    document.getElementById('adverties').style.opacity = '0.4';
+    document.getElementById('trends').style.opacity = '0.4';
+    document.getElementById('footer').style.opacity = '0.4';
+    document.getElementById('coupon').style.opacity = '1';
+}
+
+function closeCoupon() {
+    document.getElementById('coupon').style.visibility = 'hidden';
+    //document.getElementById('main9').style.opacity = '1'
+    document.getElementById('nav').style.opacity = '1';
+    document.getElementById('roop').style.opacity = '1';
+    document.getElementById('categories').style.opacity = '1';
+    document.getElementById('veriouscategories').style.opacity = '1';
+    document.getElementById('collection').style.opacity = '1';
+    document.getElementById('offervideo').style.opacity = '1';
+    document.getElementById('adverties').style.opacity = '1';
+    document.getElementById('trends').style.opacity = '1';
+    document.getElementById('footer').style.opacity = '1';
+}
